d219c0d66cf567f1625157b6b065c096ac0f9a3d
Mark Enright <enright.mark@gmail.com>
2019-08-13 18:34:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,10 +11,10 @@ end
 end
 
 
-def countdown_with_sleep(n, sleep_time)
+def countdown_with_sleep(n)
   while n > 0
   puts "#{n} SECOND(S)!"
-  sleep(sleep_time)
+  sleep(1)
   n -= 1
 end
 "HAPPY NEW YEAR!"
